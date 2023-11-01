63d915cecca6dadff6c4306bd804562e4b0c26db
suleymanerim1 <erim_suleyman@hotmail.com>
2023-11-01 15:19:10 +0300

    ad creator added

---
+++ ad_creator.py
@@ -0,0 +1,177 @@
+from PIL import Image, ImageDraw, ImageFont
+from diffusers import StableDiffusionImg2ImgPipeline
+
+
+
+def add_rounded_corners(img, radius):
+    # Create a mask
+    mask = Image.new('L', img.size, 0)
+    draw = ImageDraw.Draw(mask)
+    width, height = img.size
+
+    # Draw rounded corners on the mask
+    draw.pieslice((0, 0, radius * 2, radius * 2), 180, 270, fill=255)
+    draw.pieslice((0, height - radius * 2, radius * 2, height), 90, 180, fill=255)
+    draw.pieslice((width - radius * 2, height - radius * 2, width, height), 0, 90, fill=255)
+    draw.pieslice((width - radius * 2, 0, width, radius * 2), 270, 360, fill=255)
+    draw.rectangle((radius, 0, width - radius, height), fill=255)
+    draw.rectangle((0, radius, width, height - radius), fill=255)
+
+    # Add the mask to the image
+    img.putalpha(mask)
+    return img
+
+# Function to wrap text
+def text_wrap(text, font, max_width):
+    lines = []
+    if font.getsize(text)[0] <= max_width:
+        lines.append(text)
+    else:
+        words = text.split(' ')
+        i = 0
+        while i < len(words):
+            line = ''
+            while i < len(words) and font.getsize(line + words[i])[0] <= max_width:
+                line = line + words[i] + ' '
+                i += 1
+            lines.append(line)
+    return lines
+
+
+# Resize image proportional to original dimension
+def arrange_width(img, new_height):
+    width, height = img.size
+
+    # Calculate the proportional width based on the new height
+    ratio = new_height / height
+    new_width = int(width * ratio)
+
+    return new_width
+
+
+def resize_proportional(img, new_height):
+    try:
+        width, height = img.size
+
+        # Calculate the proportional width based on the new height
+        ratio = new_height / height
+        new_width = int(width * ratio)
+
+        # Resize the image with the new width and height
+        resized_img = img.resize((new_width, new_height))
+        return resized_img
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+def create_ad_template(main_img, logo_img, color, punchline_text, button_text, ):
+
+    # Create a blank image for the ad template
+    width, height = 500, 700  # Set the width and height of the ad template
+    ad_template = Image.new('RGB', (width, height), color='white')
+
+    logo_height = 80  # Adjust according to the logo's height
+    logo_img = resize_proportional(img= logo_img, new_height=logo_height)
+    main_image_height = 300  # Adjust according to the main image's height
+    main_img = resize_proportional(img=main_img , new_height=main_image_height)
+
+
+    # Calculate vertical gaps and positions to center the elements
+    gap = 20  # Gap between elements
+    punchline_font_size = 25  # Adjust the font size
+    button_font_size = 15  # Adjust the font size
+
+    total_height = logo_height + gap + main_image_height + gap + 4 * gap  # Total height of logo, main image, text, and button
+    start_y = (height - total_height) // 2 - gap*2
+
+    # Calculate positions to center the elements with gaps
+    logo_position = (int((width - logo_img.width) / 2), start_y)
+    main_image_position = (int((width - main_img.width) / 2), start_y + logo_height + gap)
+
+    # Paste the brand image and the main image onto the ad template
+    logo_img_rounded = add_rounded_corners(logo_img,10)
+    main_image_rounded = add_rounded_corners(main_img, 50)
+    ad_template.paste(im=logo_img_rounded, box=logo_position,mask=logo_img_rounded)
+    ad_template.paste(im=main_image_rounded, box=main_image_position,mask=main_image_rounded)
+
+    # Draw punchline text
+    draw = ImageDraw.Draw(ad_template)
+    punchline_font = ImageFont.truetype("arial.ttf", punchline_font_size)
+    button_font = ImageFont.truetype("arial.ttf", button_font_size)
+
+    # Calculate the text height for positioning
+    text_position = start_y + logo_height + 2 * gap + main_image_height
+
+    # Wrap punchline text to fit within the width
+    max_width = width - 40  # Adjust according to design
+    lines = text_wrap(punchline_text, punchline_font, max_width)
+    for line in lines:
+        text_width, text_height = draw.textsize(line, font=punchline_font)
+        draw.text(((width - text_width) / 2, text_position), line, fill=color, font=punchline_font)
+        text_position += text_height
+
+    # Draw button with specified color and white text
+    button_text_width, button_text_height = draw.textsize(button_text, font=button_font)
+    button_width = button_text_width + 20  # Adjust the button width based on the text
+    button_height = button_text_height + 10  # Adjust the button height based on the text
+    button_position = ((width - button_width) // 2, text_position + gap)
+
+    # Draw a filled rectangle (button) behind the text
+    draw.rectangle([button_position, (button_position[0] + button_width, button_position[1] + button_height)], fill=color)
+
+    # Calculate the text position to center it within the button
+    text_position = ((width - button_text_width) // 2, button_position[1] + 5)  # Adjust the Y coordinate for centering
+
+    # Draw the button text in white color
+    draw.text(text_position, button_text, fill='white', font=button_font)
+
+
+    # Save the ad template to a file
+    ad_template.save('ad_template.png')
+
+
+    print("----------The ad is produced!----------")
+
+    return ad_template
+
+
+
+
+def produce_diffusion_image(image: Image.Image, prompt: str, color_name:str):
+
+
+    model_id_or_path = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+        model_id_or_path,
+        use_auth_token="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",
+        safety_checker=None
+    )
+
+    prompt_with_color = prompt + ", " + "use color " + color_name
+
+    init_image = image.resize((512, 512))
+    created_image = pipe(prompt=prompt_with_color,
+                         image=init_image,
+                         strength=0.1,
+                         num_inference_steps=50,
+                         guidance_scale=5).images[0]
+
+    return created_image
+
+
+
+
+
+if __name__ == '__main__':
+
+    # Example usage
+    logo_image = Image.open('logo.png')
+    main_image = Image.open('image.png')
+    color = '#ff0000'
+    punchline_text = 'Your long punchline text goes here and it should wrap to multiple lines if it exceeds the specified width '
+    button_text = 'Click Here'
+
+    create_ad_template(main_image, logo_image, color, punchline_text, button_text)
--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 from PIL import Image
 from io import BytesIO
-from diffusers import StableDiffusionImg2ImgPipeline
 from webcolors import hex_to_name  # https://pypi.org/project/webcolors/1.3/
+from ad_creator import create_ad_template, produce_diffusion_image
 
+
+#TODO: webcolors library does not include many html hex code , check it
+#TODO:
 
 # to run app : uvicorn main:app --reload
 app = FastAPI()
-
-
-def produce_diffusion_image(image: Image.Image, prompt: str, color_name:str):
-
-
-
-    model_id_or_path = "runwayml/stable-diffusion-v1-5"
-    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-        model_id_or_path,
-        use_auth_token="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",
-        safety_checker=None
-    )
-
-    prompt_with_color = prompt + ", " + "use color " + color_name
-
-    init_image = image.resize((512, 512))
-    created_image = pipe(prompt=prompt_with_color,
-                         image=init_image,
-                         strength=0.1,
-                         num_inference_steps=50,
-                         guidance_scale=5).images[0]
-
-    return created_image
-
 
 
 @app.post("/task1/")
@@ -40,7 +19,13 @@ async def create_upload_file(file: UploadFile = File(...), prompt: str = Form(..
     img = Image.open(BytesIO(await file.read()))
 
     # hex codes can be looked up from here : https://www.quackit.com/css/css_color_codes.cfm
-    color_name = hex_to_name(hex_value=hex_code, spec='css3') # format : #FF0000
+
+    try:
+        color_name = hex_to_name(hex_value=hex_code, spec='css3')  # Format: #FF0000
+    except Exception as e:
+        error_message = "Hex code is not found in webcolors library"
+        return {"error": error_message}
+
     print(color_name)
 
     # Apply manipulation to the image
@@ -61,9 +46,25 @@ async def create_upload_file(file: UploadFile = File(...), prompt: str = Form(..
 async def create_upload_file(logo: UploadFile = File(...),color_hex_code: str = Form(...),
                              punch_line: str = Form(...), button:str = Form(...) ):
 
-   return StreamingResponse(img_byte_array, media_type="image/png")
+    logo_img = Image.open(BytesIO(await logo.read()))
+    main_image = Image.open('image.png')
+
+
+    ad_template = create_ad_template(main_image, logo_img, color_hex_code, punch_line, button)
+
+    # Save the modified image to a BytesIO object
+    img_byte_array = BytesIO()
+    ad_template.save(img_byte_array, format="PNG")
+    img_byte_array.seek(0)
+
+
+    # Return the ad template as a response
+    return StreamingResponse(content=img_byte_array, media_type="image/png")
+
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
